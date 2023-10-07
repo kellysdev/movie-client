25a29ae00013e81b246b78c30a65b0386d1b639d
Kelly Scott <116105463+lladysmall@users.noreply.github.com>
2023-10-07 15:04:14 -0700

    Install ESLint

---
+++ eslint.config.js
@@ -0,0 +1,13 @@
+export default [
+  {
+    "env": {
+        "browser": true,
+        "es2021": true
+    },
+    "extends": 
+      ["eslint:recommended"],
+    "rules": {
+        "quotes": ["error", "double"]
+    }
+  }
+];
