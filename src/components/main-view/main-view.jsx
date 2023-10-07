9a3fd7941be88f283273ba3e2d9979624f0fc1a5
Kelly Scott <116105463+lladysmall@users.noreply.github.com>
2023-10-07 10:28:38 -0700

    Import LogInView and return component if user isn't logged in

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,17 +1,22 @@
 import { useState, useEffect } from "react";
 import { MovieCard } from "../movie-card/movie-card";
 import { MovieView } from "../movie-view/movie-view";
+import { LoginView } from "../login-view/login-view";
 
 const MainView = () => {
   const [movies, setMovies] = useState([]);
-
   const [selectedMovie, setSelectedMovie] = useState(null);
+  const [user, setUser] = useState(null);
 
   useEffect(() => {
     fetch("https://popopolis-f7a904c7cad0.herokuapp.com/movies")
     .then((response) => response.json())
     .then((data) => {setMovies(data);});
   }, []);
+
+  if (!user) {
+    return <LoginView />;
+  }
 
   if (selectedMovie) {
     return (
