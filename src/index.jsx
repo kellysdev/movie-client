536d8e6d7a2f54329469fafef94a639fe5dc8cb9
Kelly Scott <116105463+lladysmall@users.noreply.github.com>
2023-10-13 07:55:32 -0700

    wrap MainView in a container

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import MainView from "./components/main-view/main-view";
+import Container from "react-bootstrap/Container";
 
 // Import statement to indicate that you need to bundle ".index.scss"
 import "./index.scss";
@@ -17,6 +18,8 @@ const root = createRoot(container);
 // Tells React to render your app in the root DOM element
 root.render(
   <StrictMode>
-    <MyFlixApplication />
+    <Container>
+      <MyFlixApplication />
+    </Container>
   </StrictMode>
 );
