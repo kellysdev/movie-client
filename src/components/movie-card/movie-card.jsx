4b4c204ea87b94f4e2ab994ca37de42c19123cb8
Kelly Scott <116105463+lladysmall@users.noreply.github.com>
2023-10-13 10:40:33 -0700

    Turn MovieCard element into Bootstrap Card elements

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -1,14 +1,15 @@
 import PropTypes from "prop-types";
+import { Button, Card } from "react-bootstrap";
 
 export const MovieCard = ({ movie, onMovieClick }) => {
   return (
-    <div
-      onClick={() => {
-        onMovieClick(movie);
-      }}
-    >
-      {movie.Title}
-    </div>
+    <Card className="h-100">
+      <Card.Img variant="top" src={movie.ImagePath} />
+      <Card.Title>{movie.Title}</Card.Title>
+      <Button onClick={() => {onMovieClick(movie);}}>
+        Open
+      </Button>
+    </Card>
   );
 };
 
